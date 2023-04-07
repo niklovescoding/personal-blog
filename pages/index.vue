5c2f184086c39c955719d007d759193f78c45300
nikhil.k <nikhil.k@greyb.com>
2023-04-07 12:05:18 +0530

    Fixed footer margin

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <Intro class="m-8 lg:m-20 text-center text-lg" />
   <Posts />
-  <Socials class="ml-auto" />
+  <Socials class="m-20" />
 </template>
 <script setup lang="ts">
 useHead({
